ddfe0e59cd04b704fb25dddcf01ab914d21350ef
Nicholas Vachon <nicholasvachon@hotmail.com>
2019-12-03 11:24:31 -0500

    Added a print

--- Data/PreProcessing/prepro_Chrono_CF2_MLP_dot_incl_BERT_in_RT.py
+++ Data/PreProcessing/prepro_Chrono_CF2_MLP_dot_incl_BERT_in_RT.py
@@ -10,3 +10,34 @@ Adding the BERT averaged representation to the RT
 @author: nicholas
 """
 
+
+from tqdm import tqdm
+import pandas as pd
+import torch
+from transformers import BertConfig, BertModel, BertTokenizer
+from BERTifying import Text_to_BERT_avrg
+
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-base-uncased')
+
+
+df_user = pd.read_csv('/Users/nicholas/ReDial_CF2_MLP_dot/Data/user_chrono_RT.csv')
+df_item = pd.read_csv('/Users/nicholas/ReDial_CF2_MLP_dot/Data/movie_genres_RT.csv')
+
+
+#%%
+
+
+# BERT_avrg = [Text_to_BERT_avrg(text) for text in df_user['text']]
+
+BERT_avrg = []
+for text in tqdm(df_user['text']):
+    BERT_avrg.append(Text_to_BERT_avrg(text))
+    
+
+#%%
+
+df_user['BERT_user'] = BERT_avrg
+
+print(df_user.head())
