863e652476783c286a063288918cee380d0f9c3b
Nicholas Vachon <nicholasvachon@hotmail.com>
2019-12-13 14:51:17 -0500

    Correcting the 100.csv files. Now they randomly choose not rated movies. And movies in Data/ReDial are all in ReDOrID.

--- Data/PreProcessing/Data_to_ReDial_only.py
+++ Data/PreProcessing/Data_to_ReDial_only.py
@@ -35,13 +35,13 @@ PATH_from = '/Users/nicholas/ReDial_CF2_MLP_dot/Data/DataReDialML/'
 files = [
          'Train_UNIQUE.csv',
          'Train_LIST.csv',
-         'Train100.csv',
+       #  'Train100.csv',    CAN'T BE PROCESSED HERE BECAUSE 100 RANDOM OUT OF ReDOrId 
          'Val_UNIQUE.csv',
          'Val_LIST.csv',
-         'Val100.csv',
+       #  'Val100.csv',      CAN'T BE PROCESSED HERE BECAUSE 100 RANDOM OUT OF ReDOrId 
          'Test_UNIQUE.csv',
          'Test_LIST.csv',
-         'Test100.csv'
+       #  'Test100.csv'      CAN'T BE PROCESSED HERE BECAUSE 100 RANDOM OUT OF ReDOrId 
         ]
 PATH_to = '/Users/nicholas/ReDial_CF2_MLP_dot/Data/DataReDial/'
 
@@ -62,13 +62,13 @@ for file in files:
     
     # Change UiDs to ReDOrId
     for i in range(len(data_np)):
-        # If it's a UNIQUE or 100:
-        if isinstance(data_np[i,4], int):
-            data_np[i,4] = UiD_2_ReDOrId[data_np[i,4]]
         # If it's the LIST case:
         if isinstance(data_np[i,4], str):
             l_items = ast.literal_eval(data_np[i,4])
             data_np[i,4] = [UiD_2_ReDOrId[item_id] for item_id in l_items]
+        else:
+            data_np[i,4] = UiD_2_ReDOrId[data_np[i,4]]
+
             
     # Save the new file
     new_df = pd.DataFrame(data=data_np,    # values
@@ -80,30 +80,30 @@ for file in files:
 #%%    
     
 
-"""
+# """
 
-            TREAT RELATIONAL TABLES (RT)
+#             TREAT RELATIONAL TABLES (RT)
 
             
-"""
+# """
 
-#%%
+# #%%
 
-import torch
+# import torch
 
-RT = torch.load('/Users/nicholas/ReDial_CF2_MLP_dot/Data/DataReDialML/embed_MovieAbstract_with_BERT_avrg.pt')
+# RT = torch.load('/Users/nicholas/ReDial_CF2_MLP_dot/Data/DataReDialML/embed_MovieAbstract_with_BERT_avrg.pt')
 
 
-#%%
+# #%%
 
-new_RT = torch.zeros(len(UiD_2_ReDOrId), RT.shape[1])
+# new_RT = torch.zeros(len(UiD_2_ReDOrId), RT.shape[1])
 
-for k, v in UiD_2_ReDOrId.items():
-    new_RT[v] = RT[k]
+# for k, v in UiD_2_ReDOrId.items():
+#     new_RT[v] = RT[k]
 
-#%%
+# #%%
     
-torch.save(new_RT, '/Users/nicholas/ReDial_CF2_MLP_dot/Data/DataReDial/embed_MovieAbstract_with_BERT_avrg.pt')
+# torch.save(new_RT, '/Users/nicholas/ReDial_CF2_MLP_dot/Data/DataReDial/embed_MovieAbstract_with_BERT_avrg.pt')
 
 
 
