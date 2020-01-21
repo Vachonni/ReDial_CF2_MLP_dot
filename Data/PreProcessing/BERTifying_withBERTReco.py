1eb608fd9f55ad9acd4460bb0f2355debc751721
Nicholas Vachon <nicholasvachon@hotmail.com>
2020-01-21 09:50:11 -0500

    file to create embeds obtained with BERT fined tuned from BERTReco

---
+++ Data/PreProcessing/BERTifying_withBERTReco.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  3 07:45:39 2019
+
+
+Turning BERT inputs into BERT pooler representations... 
+
+
+        ...FINE TUNED with BertForSequenceClassification used for Recommendation
+                                                                (Softmax on 48272)
+
+
+@author: nicholas
+"""
+
+
+# Get BERT pooler value from pre-trained BERTReco
+
+import numpy as np
+import torch
+from transformers import BertForSequenceClassification
+ 
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('DEVICE = ', DEVICE )
+
+
+
+model = BertForSequenceClassification.from_pretrained('/Users/nicholas/ReDial_A19/Results/ChronoTextSRGenres_W20/model_out')
+model.to(DEVICE)
+model.eval()
+print('model loaded')
+
+
+#%%
+
+# Split the different children of the model
+
+generator_of_models_modules = model.named_children()
+
+dict_of_models_modules = {}
+
+for name, module in generator_of_models_modules:
+    dict_of_models_modules[name] = module
+
+    
+#%%
+
+# Load data. BERT inputs format of user Chrono
+    
+data_user = np.load('/Users/nicholas/ReDial_CF2_MLP_dot/Data/DataReDial/BERT_input_UserChrono_dict.npy', \
+                    allow_pickle=True).item()
+
+#%%
+    
+def batch(d):
+    """
+    But dict of tensor in 1D to (1,-1) dimension 
+    for BERT input of batch size 1 equivalent
+    """
+    for k,v in d.items():
+        d[k] = d[k].view(1,-1)
+        
+    return d    
+    
+#%%    
+
+# Treat all data and put result in a RT (torch tensor)
+
+embed_UserChrono_BERTReco_FineTuned = torch.zeros(len(data_user), 768)
+
+for k, v in data_user.items():
+    v = batch(v)
+    embed_UserChrono_BERTReco_FineTuned[k] = dict_of_models_modules['bert'](**v)[1]
+    if k % 1000 == 0: print(k) 
+    
+    
+#%%    
+    
+# Save 
+
+torch.save(embed_UserChrono_BERTReco_FineTuned, 'embed_UserChrono_BERTReco_FineTuned.pt')
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
